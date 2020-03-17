0674aefc4c154d952a055a83af89a7339c10c898
Jethan16 <ethan.nel.jones@gmail.com>
2020-03-17 16:26:27 -0400

    fixed build issue

--- public/accordion/accordion.js
+++ public/accordion/accordion.js
@@ -1,44 +1,44 @@
-const boxOne = $("#box1")
-const boxTwo = $("#box2")
-const boxThree = $("#box3")
-const menuBtn = $(".logo")
+const boxOne = $("#box1");
+const boxTwo = $("#box2");
+const boxThree = $("#box3");
+const menuBtn = $(".logo");
 
-const Numlist = $(".Num-list")
-const Strlist = $(".Str-list")
-const Arrlist = $(".Arr-list")
+const Numlist = $(".Num-list");
+const Strlist = $(".Str-list");
+const Arrlist = $(".Arr-list");
 
 $(document).ready(function() {
-    $(".wrapper").hide()
-})
+  $(".wrapper").hide();
+});
 
-$(".logo").on('click', function() {
-    $(".bar").toggleClass('burgerOpen')
-})
+$(".logo").on("click", function() {
+  $(".bar").toggleClass("burgerOpen");
+});
 
-menuBtn.on('click', function() {
-    $(".wrapper").slideToggle(200, 'swing')
-})
+menuBtn.on("click", function() {
+  $(".wrapper").slideToggle(200, "swing");
+});
 
-boxOne.on('click', function() {
-    console.log('1 clicked!');
-    Numlist.slideToggle(250, 'swing' )
-    Strlist.slideUp(250, 'swing' )
-    Arrlist.slideUp(250, 'swing' )
-})
+boxOne.on("click", function() {
+  console.log("1 clicked!");
+  Numlist.slideToggle(250, "swing");
+  Strlist.slideUp(250, "swing");
+  Arrlist.slideUp(250, "swing");
+});
 
-boxTwo.on('click', function() {
-    console.log('2 clicked!');
-    Strlist.slideToggle(250, 'swing' )
-    Arrlist.slideUp(250, 'swing' )
-    Numlist.slideUp(250, 'swing' )
-})
+boxTwo.on("click", function() {
+  console.log("2 clicked!");
+  Strlist.slideToggle(250, "swing");
+  Arrlist.slideUp(250, "swing");
+  Numlist.slideUp(250, "swing");
+});
 
-boxThree.on('click', function() {
-    console.log('3 clicked!');
-    Arrlist.slideToggle(250, 'swing' )
-    Numlist.slideUp(250, 'swing' )
-    Strlist.slideUp(250, 'swing' )
-})
+boxThree.on("click", function() {
+  console.log("3 clicked!");
+  Arrlist.slideToggle(250, "swing");
+  Numlist.slideUp(250, "swing");
+  Strlist.slideUp(250, "swing");
+});
 
 // $("button").click(function(){
 //     $("p").slideToggle(1000);
